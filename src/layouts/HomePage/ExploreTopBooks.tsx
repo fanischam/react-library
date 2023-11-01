543143b6dc42df69c4260a512cea7e46f158cb16
fanischam <fchamalidis@yahoo.com>
2023-11-01 13:16:03 +0200

    Created Navbar and top books section

---
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,19 @@
+export const ExploreTopBooks = () => {
+  return (
+    <div className='p-5 mb-4 bg-dark header'>
+      <div className='container-fluid p-5 text-white d-flex justify-content-center align-items-center'>
+        <div>
+          <h1 className='display-5 fw-bold'>Find Your Next Adventure</h1>
+          <p className='col-md-8 fs-4'>Where would you like to go next?</p>
+          <a
+            type='button'
+            href='#'
+            className='btn main-color btn-lg text-white'
+          >
+            Explore Top Books
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
